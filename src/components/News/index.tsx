2ae22a4f6045c4108df5a5e6c695d88b773359f8
mariele <mariele_rbr@hotmail.com>
2021-02-19 16:13:55 -0300

    details

--- src/components/News/index.tsx
+++ src/components/News/index.tsx
@@ -1,45 +1,34 @@
-import styled from "styled-components";
-import bitmap from "../../images/Bitmap.png";
+import { ContainerNews, SectionNew, Section } from './styles'
 
-export const ContainerNews = styled.div`
-  min-width: 635px;
-`;
-export const SectionNew = styled.div`
-  display: flex;
-`;
+import map from "../../assets/bitmap.jpg";
 
-export const Section = styled.div`
-  width: 193px;
-  margin-top: 46px;
-  margin-right: 27px;
-  h2 {
-    margin-bottom: 15px;
-  }
-`;
-const news = [
+const posts = [
   {
     title: "Life Advice Looking Through A Windowns",
     text:
       " Life is a journey of twists and turns, peaks and valleys, mountains to climb and oceans to explore.",
+    id: "1"
   },
   {
     title: "Choosing The Best Photo Printer",
     text:
       " Life is a journey of twists and turns, peaks and valleys, mountains to climb and oceans to explore.",
+    id: "2"
   },
   {
     title: "Unmatched Toner Cartridge Quality 20 Less Than Oem Price",
     text:
       " Life is a journey of twists and turns, peaks and valleys, mountains to climb and oceans to explore.",
+    id: "3"
   },
 ];
 
 export const News = () => {
   return (
     <ContainerNews>
-      <img src={bitmap} alt="" />
+      <img src={map} alt="" />
       <SectionNew>
-        {news.map((noticia) => (
+        {posts.map((noticia) => (
           <Section>
             <h2>{noticia.title}</h2>
             <p>{noticia.text}</p>
---
+++ src/components/News/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ContainerNews = styled.div`
+  min-width: 635px;
+`;
+
+export const SectionNew = styled.div`
+  display: flex;
+`;
+
+export const Section = styled.div`
+  width: 193px;
+  margin-top: 46px;
+  margin-right: 27px;
+  h2 {
+    margin-bottom: 15px;
+  }
+`;
